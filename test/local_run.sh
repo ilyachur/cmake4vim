5105e8055625c5859675437487da9c5eb7b51829
Ilya Churaev <ilya.churaev@intel.com>
2019-12-05 21:09:11 +0300

    Changed script

--- test/local_run.sh
+++ test/local_run.sh
@@ -7,7 +7,6 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_HOME=$(realpath ${CURRENT_DIR}/)
 export PLUGIN_HOME=$(realpath ${CURRENT_DIR}/../)
 export HOME="${TEST_HOME}/tmp"
-touch ${HOME}/.bashrc
 git clone --depth 1 https://github.com/junegunn/vader.vim.git ${HOME}/.vim/plugged/vader.vim
 git clone --depth 1 https://github.com/junegunn/fzf.vim ${HOME}/.vim/plugged/fzf.vim
 git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
