4386ceed57c68e074b7e9a351e9fb2b3c1e23943
Ilya Churaev <ilya.churaev@intel.com>
2020-08-28 21:34:58 +0300

    Added windows config

---
+++ test/local_run.bat
@@ -0,0 +1,12 @@
+REM  @echo off
+
+set VADER_OUTPUT_FILE=%~dp0\vader_output
+type nul > "%VADER_OUTPUT_FILE%"
+
+pushd %~dp0
+set PLUGIN_HOME=%CD%\..\
+set HOME=%CD%\tmp
+if not exist %HOME% mkdir %HOME%
+git clone --depth 1 https://github.com/junegunn/vader.vim.git %HOME%\.vim\plugged\vader.vim
+git clone --depth 1 https://github.com/tpope/vim-dispatch.git %HOME%\.vim\plugged\vim-dispatch
+vim -Nu .vimrc +Vader!*
