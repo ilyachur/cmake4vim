8296938000985e74367b2d18bd464965f4312f9c
Ilya Churaev <ilya.churaev@intel.com>
2020-10-31 07:22:19 +0300

    Try to use shell script for vim and nVim

--- test/local_run.sh
+++ test/local_run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+export VIM=vim
+
+if [ "$1"  ]
+then
+    export VIM=$1
+fi
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_HOME=$(realpath ${CURRENT_DIR}/)
 export PLUGIN_HOME=$(realpath ${CURRENT_DIR}/../)
 export HOME="${TEST_HOME}/tmp"
 git clone --depth 1 https://github.com/junegunn/vader.vim.git ${HOME}/.vim/plugged/vader.vim
 git clone --depth 1 https://github.com/tpope/vim-dispatch.git ${HOME}/.vim/plugged/vim-dispatch
-rm -f ${HOME}/vim-profile.txt
+# rm -f ${HOME}/vim-profile.txt
 # git clone --depth 1 https://github.com/junegunn/fzf.vim ${HOME}/.vim/plugged/fzf.vim
 # git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
 # ${HOME}/.fzf/install --all
 
 cd ${TEST_HOME}
-eval "vim -Nu vimrc -c 'Vader! *'"
+eval "${VIM} -Nu vimrc -c 'Vader! *'"
