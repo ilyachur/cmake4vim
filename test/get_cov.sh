bb52a4932f1d96ac685bb33d0c863566eb23db44
Ilya Churaev <ilyachur@gmail.com>
2020-11-04 23:54:58 +0300

    Removed obsolete scripts (#48)

--- test/get_cov.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TEST_HOME=$(realpath ${CURRENT_DIR}/)
-export PLUGIN_HOME=$(realpath ${CURRENT_DIR}/../)
-export HOME="${TEST_HOME}/tmp"
-
-python -m covimerage write_coverage --data-file ${PLUGIN_HOME}/.coverage_covimerage ${HOME}/vim-profile.txt
